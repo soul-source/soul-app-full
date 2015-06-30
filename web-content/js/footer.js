c7f62ef0dab2ee5fe3abe8d605cff9351fb92ba0
Yehonal <hw.2@hotmail.it>
2015-06-30 17:35:44 +0200

    implemented footer

--- web-content/js/footer.js
+++ web-content/js/footer.js
@@ -4,25 +4,29 @@ hwc.define([
     var $ = this;
 
     var Footer = $.class.extends($.Browser.Component)(
-            $.public({
-                __construct: function (parent, childs, opt) {
-                    var template = new $.Browser.Template("soul/footer.html");
-                    opt.template = template;
+        $.public({
+            __construct: function (parent, childs, opt) {
+                var template = new $.Browser.Template("soul/footer.html");
+                opt.template = template;
 
-                    this.__super(parent, [], opt);
-                },
-                update: function () {
-                    this.__super();
-                },
-                init: function () {
-                    var that = this;
-                    this.__super().then(function () {
-                    });
-                },
-                build: function () {
-                    this.__super();
-                }
-            }));
+                this.__super(parent, [], opt);
+            },
+            update: function () {
+                this.__super();
+            },
+            init: function () {
+                var that = this;
+                this.__super().then(function () {
+                    that.i.getRouter().setRoute("#report-short-link", {component: "report"});
+                });
+            },
+            build: function () {
+                this.__super();
+                var page = this.i.getRouter().getRouteInfo().getComponent();
+                var footer = $.Browser.JQ(".hwc-footer-inner");
+                page === "home" ? footer.hide() : footer.show();
+            }
+        }));
 
     return Footer;
 });
